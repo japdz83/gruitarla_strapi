c26b59042536c067939f23d194f221e4f0c43f67
Johan Prado <japdz83@gmail.com>
2022-06-28 10:16:49 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f65195e43642f9b406e183b5cf967b7b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '712e982b-7500-449c-b250-b134ba2ce805'
+};
